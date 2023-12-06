eb1af13aab6c95ad9583c9906029e514b21bfb28
Diogo Evangelista <dhesousa77@gmail.com>
2023-12-05 21:11:54 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -6,16 +7,24 @@ export function Sidebar() {
       <img
         className={styles.cover}
         src="https://images.unsplash.com/photo-1577375729078-820d5283031c?q=50&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Foto de capa do Perfil"
+        alt="Foto de capa do perfil"
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://cdn.meutimao.com.br/_upload/idolos-do-corinthians/adenor.jpg"
+          alt="Foto de perfil"
+        />
         <strong>Diogo Henrique</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Edite seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
