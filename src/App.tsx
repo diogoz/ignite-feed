6c3dcaa641904850a4795aa8991255e6cd4302cc
Diogo Evangelista <dhesousa77@gmail.com>
2023-12-11 15:52:46 -0300

    Os motores do React - Iterando no JSX

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,45 @@ import "./global.css";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 export function App() {
+  const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: "https://github.com/diogoz.png",
+        name: "Diogo Henrique",
+        role: "Web Developer",
+      },
+      content: [
+        { type: "paragraph", content: "Fala galera 👋" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+        },
+        { type: "link", content: "jane.design/doctorcare" },
+      ],
+      publishedAt: new Date("2022-12-10 20:00:00"),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: "https://github.com/renatojfsantos.png",
+        name: "Renato Santos",
+        role: "Developer",
+      },
+      content: [
+        { type: "paragraph", content: "Fala galera 👋" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+        },
+        { type: "link", content: "jane.design/doctorcare" },
+      ],
+      publishedAt: new Date("2022-12-09 20:00:00"),
+    },
+  ];
+
   return (
     <div>
       <Header />
@@ -11,7 +50,17 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map((post) => {
+            return (
+              <div key={post.id}>
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              </div>
+            );
+          })}
         </main>
       </div>
     </div>
