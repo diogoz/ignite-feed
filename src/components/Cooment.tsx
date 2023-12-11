a1a7f2d9332ae8f0d22a2d7523d1b9e98315fc15
Diogo Evangelista <dhesousa77@gmail.com>
2023-12-11 11:04:44 -0300

    Estrutura da aplicação - Componente: Comment

---
+++ src/components/Cooment.tsx
@@ -0,0 +1,37 @@
+import styles from "./Comment.module.css";
+
+import { ThumbsUp, Trash } from "phosphor-react";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://cdn.meutimao.com.br/_upload/idolos-do-corinthians/adenor.jpg" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diogo Henrique</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
